001666eed4738b21fcffcbf200a8d781c44c9b6a
AlonsoS <ghoul6413@gmail.com>
2025-06-10 21:30:08 -0300

    testando

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
              Caneta c = new Caneta("vermelha","bic",1.5,false);
         System.out.println(c.toString());
+
+        System.out.println("so pra mudar algum bagui");
     }
 }
